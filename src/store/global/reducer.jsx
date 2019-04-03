bc6c8afb9927afdfc5ca68296b1d7a19d2ae1656
lcxdi888717 <lcxdi888717@gmail.com>
2019-04-03 23:04:57 +1100

    frp-react-init

---
+++ src/store/global/reducer.jsx
@@ -0,0 +1,10 @@
+import {} from './actionTypes';
+
+const initialState = {};
+
+const globalsReducer = (state = initialState, action) => {
+  switch (action.type) {
+  }
+};
+
+export default globalsReducer;
